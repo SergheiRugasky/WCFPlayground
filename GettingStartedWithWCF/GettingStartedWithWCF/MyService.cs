95d7f044d5612c10a964327e3266a9b61ef8a7d1
SergheiRugasky <adrianboboc2003@yahoo.com>
2015-01-09 15:36:55 +0200

    first draft

---
+++ GettingStartedWithWCF/GettingStartedWithWCF/MyService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace GettingStartedWithWCF
+{
+    [DataContract]
+    public class Name
+    {
+        [DataMember]
+        public string First { get; set; }
+        [DataMember]
+        public string Last { get; set; }
+    }
+
+    [ServiceContract]
+    public interface IMyService
+    {
+        [OperationContract]
+        string SayHello(Name personName);
+    }
+    
+    public class MyService:IMyService
+    {
+        public string SayHello(Name personName)
+        {
+            return "Hello " + personName.First + personName.Last;
+        }
+    }
+}
