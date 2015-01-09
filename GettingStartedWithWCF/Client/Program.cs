1fb72143d31dc9c4f8febd3a55079aac39d6b591
SergheiRugasky <adrianboboc2003@yahoo.com>
2015-01-09 16:19:20 +0200

    add client to demo

---
+++ GettingStartedWithWCF/Client/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ServiceModel;
+using Client.HelloMotherfucka;
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new MyServiceClient("NetTcpBinding_IMyService");
+            Name name = new Name();
+            name.First = "Ion";
+            name.Last = "Iliescu";
+            var sayHello = client.SayHello(name);
+            Console.WriteLine(sayHello);
+            Console.ReadLine();
+        }
+    }
+}
