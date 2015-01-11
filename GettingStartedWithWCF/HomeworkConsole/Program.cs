df9470f0618373c240fe8be528b18c0e62137a9e
Serghei Rugasky <adrianboboc2003@yahoo.com>
2015-01-11 20:52:48 +0200

    not working

---
+++ GettingStartedWithWCF/HomeworkConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Homework;
+
+namespace HomeworkConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var  homeworkHost = new ServiceHost(typeof(HomeworkService));
+            homeworkHost.Open();
+            Console.WriteLine("homework is running");
+            Console.ReadLine();
+        }
+    }
+}
