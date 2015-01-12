8a0247ebfae0b0ff1559ba4bc456b2bcfba917bc
SergheiRugasky <adrianboboc2003@yahoo.com>
2015-01-12 10:56:06 +0200

    working client from metadata

---
+++ ServiceCalculator/Client/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using Entities;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new CalculatorServiceClient(new BasicHttpBinding(), new EndpointAddress("http://localhost:12345"));
+            var a = new Complex {Real = 1,Imaginary = 2};
+            var b = new Complex {Real = 3,Imaginary = 4};
+            var sum = service.Add(a, b);
+            var diff = service.Substract(b, a);
+            var multiply = service.Multiply(a, b);
+            Console.WriteLine("a="+a);
+            Console.WriteLine("b="+b);
+            Console.WriteLine("sum=" + sum);
+            Console.WriteLine("diff=" + diff);
+            Console.WriteLine("multiply=" + multiply);
+            try
+            {
+                service.ThrowError();
+            }
+            catch (FaultException faultException)
+            {
+                Console.WriteLine(faultException.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ServiceCalculator/ServiceCalculator/ICalculatorService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using Entities;
+
+namespace ServiceCalculator
+{
+    [ServiceContract]
+    public interface ICalculatorService
+    {
+        [OperationContract]
+        Complex Add(Complex a, Complex b);
+
+        [OperationContract]
+        Complex Substract(Complex first, Complex second);
+
+        [OperationContract]
+        Complex Multiply(Complex first, Complex second);
+
+        [OperationContract]
+        void ThrowError();
+    }
+
+    public class CalculatorService : ICalculatorService
+    {
+        public Complex Add(Complex a, Complex b)
+        {
+            return a + b;
+        }
+
+        public Complex Substract(Complex first, Complex second)
+        {
+            return first - second;
+        }
+
+        public Complex Multiply(Complex first, Complex second)
+        {
+            return first*second;
+        }
+
+        public void ThrowError()
+        {
+            throw new FaultException("Hey asshole");
+        }
+    }
+}
